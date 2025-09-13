aba157bf5675ff3a3efd815e5a56379c41023fc8
PyMarcus <marcus-v@outlook.com>
2025-09-12 21:55:47 -0300

    feat: added css_locators

--- locators/css_locators.py
+++ locators/css_locators.py
@@ -9,3 +9,16 @@ def type_email_input(page: playwright.sync_api.Page) -> None:
 def click_enter_button(page: playwright.sync_api.Page) -> None:
     button = page.query_selector("#enterimg")
     button.click()
+
+
+def type_username_and_password_input(username: str, password: str, page: playwright.sync_api.Page) -> None:
+    input_username_txt = page.wait_for_selector('input[name="username"]')
+    input_username_txt.type(username)
+
+    input_password_txt = page.wait_for_selector('input[name="password"]')
+    input_password_txt.type(password)
+
+
+def click_login_button(page: playwright.sync_api.Page) -> None:
+    input_password_txt = page.wait_for_selector('button[type="submit"]')
+    input_password_txt.click()
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from playwright.sync_api import sync_playwright
 from locators import *
+from playwright.sync_api import sync_playwright
 
+from locators import *
 
 if __name__ == '__main__':
 
     url: str = "https://demo.automationtesting.in"
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto(url)
+    url2: str = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
 
-        type_email_input(page)
-        click_enter_button(page)
+    try:
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=False)
+            page = browser.new_page()
+            page.goto(url)
 
-        page.wait_for_timeout(10000)
+            type_email_input(page)
+            click_enter_button(page)
+
+            page.wait_for_timeout(3000)
+
+            page.goto(url2)
+
+            type_username_and_password_input("Admin", "admin123", page)
+            click_login_button(page)
+
+            page.wait_for_timeout(10000)
+    except Exception as e:
+        print(f"Error: {e}")
